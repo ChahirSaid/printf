4be8b8716ad0f85bbcd884c6ceeeb97cc1120617
ChahirSaid <chahirsaid18@gmail.com>
2023-04-23 13:48:53 +0100

    write a string

--- _putstr.c
+++ _putstr.c
@@ -5,12 +5,12 @@
  * Return: number of characters printed.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putstr(char *s) {
+int _putstr(char *s)
+{
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++) {
+	for (i = 0; s[i] != '\0'; i++)
 		write(1, &s[i], 1);
-	}
-	return i;
+	return (i);
 }
 
