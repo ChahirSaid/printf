5b7fb7d1ea2bd920c6f3ce60daecea8291e66da7
ChahirSaid <chahirsaid18@gmail.com>
2023-04-23 21:33:41 +0100

    main functions

--- main.h
+++ main.h
@@ -8,10 +8,24 @@
 #include <string.h>
 #include <limits.h>
 
+/**
+ * struct convert - structure to handle conversion specifiers
+ * @specifier: conversion specifier
+ * @f: function pointer to corresponding print function
+ */
+typedef struct convert
+{
+	char specifier;
+	int (*f)(va_list);
+} convert_t;
+
 /**--Functions--*/
-int _strlen(const char *s);
 int _printf(const char *format, ...);
-void _putchar(char c);
-void _putstr(const char *s);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_numbers(va_list args);
+int print_numbers_helper(unsigned int n);
 
 #endif
