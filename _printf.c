75d615d52a9d3d4b8b991eff93ed8e33072f16e8
ChahirSaid <chahirsaid18@gmail.com>
2023-04-23 21:55:38 +0100

    printf

---
+++ _printf.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+ * handle_format_specifier - handles the format specifier
+ * @args: va_list
+ * @format: char
+ * Return: length
+ */
+int handle_format_specifier(va_list args, char format)
+{
+	convert list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_numbers},
+		{"i", print_numbers},
+		{NULL, NULL}
+	};
+	int i, length = 0;
+	int flag = 1;
+
+	if (format == ' ' || format == '\0')
+		return (-1);
+
+	for (i = 0; list[i].type != NULL; i++)
+	{
+		if (format == list[i].type[0])
+		{
+			flag = 0;
+			length = list[i].f(args);
+		}
+	}
+	if (flag == 1)
+	{
+		_putchar('%');
+		_putchar(format);
+		length = 2;
+	}
+	return (length);
+}
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: string
+ * Return: length of string
+ */
+int _printf(const char *format, ...)
+{
+	int i, output, length = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			output = handle_format_specifier(args, format[++i]);
+			if (output == -1)
+			return (-1);
+		length += output;
+	}
+		else
+		{
+			length++;
+			_putchar(format[i]);
+		}
+	}
+	va_end(args);
+	return (length);
+}
+
---
+++ print_functions.c
@@ -0,0 +1,97 @@
+#include "main.h"
+
+/**
+ * print_char - print a single character
+ * @args: va_list
+ * Return: 1
+ */
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - print a string of characters
+ * @args: va_list
+ * Return: length of string
+ */
+int print_string(va_list args)
+{
+	int i;
+	char *s = va_arg(args, char*);
+
+	if (s == NULL)
+	s = "(null)";
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	return (i);
+}
+
+/**
+ * print_percent - print a percent sign (\% also works)
+ * @args: va_list
+ * Return: 1
+ */
+int print_percent(va_list args)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_numbers - print signed integers
+ * @args: va_list
+ * Return: length of integer
+ */
+int print_numbers(va_list args)
+{
+	int num = va_arg(args, int);
+	int count = 0;
+	int sign = 0;
+	unsigned int n;
+
+	if (num < 0)
+	{
+		sign = 1;
+		n = -num;
+		_putchar('-');
+		count++;
+	}
+	else
+	{
+		n = num;
+	}
+
+	if (n / 10)
+	{
+		count += print_numbers_helper(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+	count++;
+
+	return (count);
+}
+
+/**
+ * print_numbers_helper - helper function to recursively print numbers
+ * @n: unsigned integer
+ * Return: length of integer
+ */
+int print_numbers_helper(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+	{
+		count += print_numbers_helper(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+	count++;
+
+	return (count);
+}
+
